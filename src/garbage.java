a39010fa852fcc6a83636db379e7022f9ea2cb22
speedysebi <2ndkreisler@gmail.com>
2025-09-27 23:47:53 -0500

    this is some more stuff for a variable name checker

---
+++ src/garbage.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class garbage {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        if(input < 10_000) {
+
+        } else {
+            System.out.print("your poor");
+        }
+    }
+    for(int i=0; i < 3; i++) {
+        for(int j=10; j < 12; j++) {
+            System.out.print("(" + i + "," + j + ")");
+        }
+    }
+
+
+    Scanner in = new Scanner(source: "this 4 some\n 12.3")
+    System.out.println(in);
+    System.out.println(in.next());
+    System.out.println(in.nextInt());
+    System.out.println(in.next());
+    System.out.println(in.nexLine());
+    System.out.println(in.nextLine());
+    System.out.println(in);
+}
