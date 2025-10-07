46b01c88c27f8389f8d6168aac65719571e4f4d9
speedysebi <2ndkreisler@gmail.com>
2025-10-06 20:37:12 -0500

    original push for Growth Rate

---
+++ src/week_6/Rectangles.java
@@ -0,0 +1,17 @@
+package week_6;
+
+public class Rectangles {
+    public String units;
+    private final double length;
+    private final double width;
+
+    public Rectangles(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return 0.0;
+    }
+
+}
